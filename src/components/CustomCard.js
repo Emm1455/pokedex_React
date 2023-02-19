9f69b401753fdabe982b36b9afdec9b5f88f28db
Emmanuel Mercado <emmanuelf21004@gmail.com>
2023-02-19 01:40:50 -0400

    Center pokemon image and sort data in cardContent

--- src/components/CustomCard.js
+++ src/components/CustomCard.js
@@ -9,6 +9,7 @@ import { green, common } from '@mui/material/colors';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 import Stats from './Stats';
+import Box from '@mui/material/Box';
 
 import { v4 as uuid } from "uuid";
 
@@ -34,14 +35,14 @@ export default function MediaCard({ data, weaknesses ,img }) {
   }
 
   return (
-    <Card sx={{ maxWidth: 500, minWidth: 324, bgcolor: green[100], border: 4, borderColor: green[700], borderRadius: 2}}>
+    <Card sx={{ maxWidth: 500, bgcolor: green[100], border: 4, borderColor: green[700], borderRadius: 2}}>
       <CardHeader
         sx={{ bgcolor: green[700]}}
         avatar={
           <Avatar sx={{ bgcolor: green[100], border: 2, borderColor: common['white'] }} alt="pokemon-small" src={data.sprites.front_default}/>
         }
         title={
-          <Typography gutterBottom variant="h5" component="div">{data.name}</Typography>
+          <Typography  variant="h5" component="div">{data.name}</Typography>
           }
         subheader={
           <Stack direction="row" spacing={1}>
@@ -49,28 +50,30 @@ export default function MediaCard({ data, weaknesses ,img }) {
           </Stack>
         }
       />
-      <CardContent sx={{border: 2, borderColor: green[500], borderRadius: 2}}>
-        <CardMedia sx={{ width: 192, height:192}} image={img} title="pokemon"/>
-      </CardContent>
+      <Box sx={{ display: 'flex', justifyContent: 'center' , bgcolor: common['white']}}>
+        <CardMedia sx={{ width: 200}} height="200" component="img"  image={img} alt="pokemon"/>
+      </Box>
       <CardContent>
-      <Typography gutterBottom variant="h6" component="div">Info</Typography>
-        <Typography variant="body1" color="text.secondary">
-          While it is young, it uses the nutrients that are stored in the seed on its back in order to grow.
+        <Typography gutterBottom variant="h6" component="div" fontWeight="bold">Info</Typography>
+        <Typography paragraph>
+          While it is young, it uses the nutrients that <br/> are stored in the seed on its back in order to grow.
         </Typography>
-      </CardContent>
-      <CardContent>
-        <Typography gutterBottom variant="h6" component="div">Weight:</Typography>
-        <Typography variant="body1" color="text.secondary">{data.weight / 10} Kg</Typography>
-        <Typography gutterBottom variant="h6" component="div">Height:</Typography>
-        <Typography variant="body1" color="text.secondary">{data.height / 10} m</Typography>
-      </CardContent>
-      <CardContent>
-        <Stats pokeData={data}/>
-      </CardContent>
-      <CardContent>
-        <Stack direction="row" spacing={1}>
-          <WeaknessesDrawer weaknesses={weaknesses}/>
-        </Stack>
+        <Typography gutterBottom variant="h6" component="div"  fontWeight="bold">Weaknesses:</Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+          <Stack direction="row" spacing={2}>
+            <WeaknessesDrawer weaknesses={weaknesses}/>
+          </Stack>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-start', gap:2}}>
+          {/* <Typography gutterBottom variant="h6" component="div">Weight:</Typography> */}
+          <Typography variant="body1"> <b>Weight: </b>{data.weight / 10} Kg</Typography>
+          {/* <Typography gutterBottom variant="h6" component="div">Height:</Typography> */}
+          <Typography variant="body1"><b>Height: </b>{data.height / 10} m</Typography>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+          <Stats pokeData={data}/>
+        </Box>
+        
       </CardContent>
     </Card>
   );
